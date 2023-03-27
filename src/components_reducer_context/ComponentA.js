fad43fc5053a821a75ecd082c2867471d262c480
Kwok Yew Weng <66321555+kywleon@users.noreply.github.com>
2023-03-28 03:36:19 +0800

    Learn react-hook
    Learn how to use useReducer and useContext

--- src/components_reducer_context/ComponentA.js
+++ src/components_reducer_context/ComponentA.js
@@ -6,7 +6,7 @@ function ComponentA() {
 const countContext = useContext(CountContext)
   return (
     <div>
-      Component A
+      Component A - { countContext.countState }
       <button onClick={ () => countContext.countDispatch('increment') }>Increment</button>
       <button onClick={ () => countContext.countDispatch('decrement') }>Decrement</button>
       <button onClick={ () => countContext.countDispatch('reset') }>Reset</button>
